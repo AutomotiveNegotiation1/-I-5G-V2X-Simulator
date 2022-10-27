029e07b221c8783b9d1e2f1887a3fb761c5574a8
Sanghoon An <107896679+BabSangBab@users.noreply.github.com>
2022-10-28 02:35:29 +0900

    Add files via upload

--- blerCurvesFast.m
+++ blerCurvesFast.m
@@ -1,6 +1,6 @@
-function obj = blerCurvesFast(BLER_curves,SNR_data)
+function obj = blerCurvesFast(PER_curves,SNR_data)
            resolution = 0.05;
-           N_CQIs = length(BLER_curves);
+           N_CQIs = length(PER_curves);
            min_SINR = min(SNR_data{1});
            max_SINR = max(SNR_data{1});
            for i_=1:N_CQIs
@@ -8,22 +8,22 @@ function obj = blerCurvesFast(BLER_curves,SNR_data)
                max_SINR = max([max_SINR SNR_data{i_}]);
            end
            SINR_range  = min_SINR:resolution:max_SINR;
-           BLER_matrix = zeros(N_CQIs,length(SINR_range));
+           PER_matrix = zeros(N_CQIs,length(SINR_range));
            % Fill in the BLER data
            for i_=1:N_CQIs
                current_SNR  = SNR_data{i_};
-               current_BLER = BLER_curves{i_};
+               current_PER = PER_curves{i_};
                if current_SNR(1)>min_SINR
                    current_SNR  = [min_SINR current_SNR];
-                   current_BLER = [current_BLER(1) current_BLER];
+                   current_PER = [current_PER(1) current_PER];
                end
                if current_SNR(end)<max_SINR
                    current_SNR  = [current_SNR max_SINR];
-                   current_BLER = [current_BLER current_BLER(end)];
+                   current_PER = [current_PER current_PER(end)];
                end
-               BLER_matrix(i_,:) = interp1(current_SNR,current_BLER,SINR_range);
+               PER_matrix(i_,:) = interp1(current_SNR,current_PER,SINR_range);
            end
-           obj.BLER_data   = BLER_matrix';
+           obj.PER_data   = PER_matrix';
            obj.SNR_data    = SINR_range;
            obj.SINR_range  = [min_SINR max_SINR];
            obj.multipliers = reshape(0:length(SINR_range):length(SINR_range)*(N_CQIs-1),[],1);
